ee58cd47fed8c8e0a934aebc38f20e424432d9b1
Mai Vang <vmaineng@gmail.com>
2022-04-28 09:44:08 -0700

    adding Home Team

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -25,3 +25,129 @@ function playerStats(name) {
 function bigShoeRebounds() {
     //returns the number of rebounds of the player with the largest shoe size
 }
+
+
+const gameObject = () => {
+    return game = {
+    home: {
+        teamName: "Brooklyn Nets"
+        colors: [
+            { colors: "black",
+            colors: "white"
+            }
+        ]
+        players: {
+            "Alan Anderson":
+                {number: "0",
+                Shoe: "16",
+                points: "22",
+                rebounds:  "12",
+                assists: "12",
+                steals: "3",
+                blocks: "1",
+                slamDunks: "1",
+                }
+             "Reggie Evans":
+                {number: "30",
+                shoe: "14",
+                points: "12",
+                rebounds:  "12",
+                assists: "12",
+                steals: "12",
+                blocks: "12",
+                slamDunks: "7",
+                }
+            "Brook Lopez":
+                {number: "11",
+                shoe: "17",
+                points: "17",
+                rebounds:  "19",
+                assists: "10",
+                steals: "3",
+                blocks: "1",
+                slamDunks: "15",
+                }
+            "Mason Plumlee":
+                {number: "1",
+                shoe: "19",
+                points: "26",
+                rebounds:  "12",
+                assists: "6",
+                steals: "3",
+                blocks: "8",
+                slamDunks: "5",
+                }
+            "Jason Terry":
+                {number: "31",
+                shoe: "15",
+                points: "19",
+                rebounds:  "2",
+                assists: "2",
+                steals: "4",
+                blocks: "11",
+                slamDunks: "1",
+                }
+        }
+    }
+    away: {
+        teamName: "Charlotte Hornets"
+        colors: [
+            {colors: "Turquoise",
+            colors: "Purple"
+            }
+
+        ]
+        players: {
+            "Jeff Adrien":
+            {number: "31",
+            shoe: "15",
+            points: "19",
+            rebounds:  "2",
+            assists: "2",
+            steals: "4",
+            blocks: "11",
+            slamDunks: "1",
+            }
+         "Bismak Biyombo":
+            {number: "31",
+            shoe: "15",
+            points: "19",
+            rebounds:  "2",
+            assists: "2",
+            steals: "4",
+            blocks: "11",
+            slamDunks: "1",
+            }
+        "DeSagna Diop":
+            {number: "31",
+            shoe: "15",
+            points: "19",
+            rebounds:  "2",
+            assists: "2",
+            steals: "4",
+            blocks: "11",
+            slamDunks: "1",
+            }
+        "Ben Gordon":
+            {number: "31",
+            shoe: "15",
+            points: "19",
+            rebounds:  "2",
+            assists: "2",
+            steals: "4",
+            blocks: "11",
+            slamDunks: "1",
+            }
+        "Brendan Haywood":
+            {number: "31",
+            shoe: "15",
+            points: "19",
+            rebounds:  "2",
+            assists: "2",
+            steals: "4",
+            blocks: "11",
+            slamDunks: "1",
+            }
+        }
+    }
+}
