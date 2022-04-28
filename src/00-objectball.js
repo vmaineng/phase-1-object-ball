2cc4e8fc0e148eaddfa5495d7a291e83cc7343bb
Eli Craig <elidcraig@gmail.com>
2022-04-28 08:55:15 -0700

    Add function declarations

---
+++ src/00-objectball.js
@@ -0,0 +1,27 @@
+function numPointsScored(name) {
+    //returns the number of points scored by that player
+}
+
+function shoeSize(name) {
+    //returns shoe size of player
+}
+
+function teamColors(team) {
+    //returns array of team's colors
+}
+
+function teamNames() {
+    //returns an array of team names
+}
+
+function playerNumbers(team) {
+    //returns an array of jersey numbers for team
+}
+
+function playerStats(name) {
+    //returns an object of player's stats
+}
+
+function bigShoeRebounds() {
+    //returns the number of rebounds of the player with the largest shoe size
+}
